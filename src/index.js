f88d3db32811316ead3d33b24cc0a0a7f57733b2
hank <hankchiu.tw@gmail.com>
2019-05-05 22:27:46 +0800

    ready to use lit-element

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
-import {Icon} from "@material/mwc-icon"
+import { LitElement, html } from 'lit-element'
+import { Icon } from '@material/mwc-icon'
 
 console.log('Icon:', Icon)
-// document.body.querySelector('#app').appendChild()
+
+class MyElement extends LitElement {
+  /**
+   * Implement `render` to define a template for your element.
+   *
+   * You must provide an implementation of `render` for any element
+   * that uses LitElement as a base class.
+   */
+  render() {
+    /**
+     * `render` must return a lit-html `TemplateResult`.
+     *
+     * To create a `TemplateResult`, tag a JavaScript template literal
+     * with the `html` helper function:
+     */
+    return html`
+      <!-- template content -->
+      <p>A paragraph</p>
+    `
+  }
+}
+// Register the new element with the browser.
+customElements.define('my-element', MyElement)
