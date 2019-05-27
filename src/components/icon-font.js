caab1677ecd3b9017235ace44429eb19a531d85c
hank <hankchiu.tw@gmail.com>
2019-05-27 21:52:12 +0800

    update icon fonts

--- src/components/icon-font.js
+++ src/components/icon-font.js
@@ -9,35 +9,50 @@ class IconFont extends LitElement {
 
   static get styles() {
     return css`
-    :host {
-      font-family: 'icomoon' !important;
-      width: 100%;
-      height: 100%;
-      display: flex;
-    }
+      :host {
+        font-family: "icomoon" !important;
+        width: 100%;
+        height: 100%;
+        display: flex;
+      }
 
-    [class^='icon-'] {
-      margin: auto;
-    }
+      [class^="icon-"] {
+        margin: auto;
+      }
 
-    .icon-github:before {
-      content: "\\eab0";
-    }
-    .icon-linkedin:before {
-      content: "\\eaca";
-    }
-    .icon-linkedin2:before {
-      content: "\\eac9";
-    }
-    .icon-stackoverflow:before {
-      content: "\\ead0";
-    }
+      .icon-paint-format:before {
+        content: "\\e90c";
+      }
+      .icon-stack:before {
+        content: "\\e92e";
+      }
+      .icon-bug:before {
+        content: "\\e999";
+      }
+      .icon-rocket:before {
+        content: "\\e9a5";
+      }
+      .icon-embed:before {
+        content: "\\ea80";
+      }
+      .icon-terminal:before {
+        content: "\\ea81";
+      }
+      .icon-github:before {
+        content: "\\eab0";
+      }
+      .icon-linkedin:before {
+        content: "\\eaca";
+      }
+      .icon-stackoverflow:before {
+        content: "\\ead0";
+      }
     `
   }
 
   render() {
     return html`
-    <div class='icon-${this.icon}'></div>
+      <div class="icon-${this.icon}"></div>
     `
   }
 }
