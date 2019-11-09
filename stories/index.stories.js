2804fec9c4dc71d02a700d472e56e0fa09003266
hank <hankchiu.tw@gmail.com>
2019-11-09 17:25:48 +0800

    Upgrade storybook

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,14 +1,8 @@
 import { document, console } from 'global'
-import { storiesOf } from '@storybook/html'
-import '../src/components/profile-card.js'
-import resumeJSON from '../assets/resume.json'
+import '../hankchiutw-webcomponent/src/components/app-root.js'
 
-storiesOf('Basic', module).add('profile-card', () => {
-  console.log('resumeJSON:', resumeJSON)
-  return `
-  <profile-card
-    imgurl='assets/portrait.jpg'
-    resumejson='${JSON.stringify(resumeJSON)}'
-  ></profile-card>
-  `
-})
+export default { title: 'Workspaces' }
+
+export const webcomponents = () => {
+  return '<app-root></app-root>'
+}
