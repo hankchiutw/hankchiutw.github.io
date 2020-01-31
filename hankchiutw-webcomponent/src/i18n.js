0bbd0718da61fc0b94bcf3d03de0dadc7bf054a9
hank <hankchiu.tw@gmail.com>
2020-01-31 09:49:09 +0800

    [webcomponent] Load locale files from github gists

--- hankchiutw-webcomponent/src/i18n.js
+++ hankchiutw-webcomponent/src/i18n.js
@@ -1,12 +1,40 @@
-import i18next from 'i18next'
-import XHR from 'i18next-xhr-backend'
+import i18next from 'i18next';
+import XHR from 'i18next-xhr-backend';
+import axios from 'axios';
 
-export const _ = i18next.t.bind(i18next)
+export const _ = i18next.t.bind(i18next);
 
-export const i18nReady = i18next.use(XHR).init({
-  debug: false,
-  lng: 'en',
-  fallbackLng: 'en',
-  ns: ['translation', 'resume'],
-  defaultNS: 'translation',
-})
+const gistID = 'i18next';
+const validNS = ['translation', 'resume'];
+async function getJSONMap() {
+  const res = await axios.get('https://api.github.com/users/hankchiutw/gists');
+  const gist = res.data.find(({ description }) => description === gistID);
+
+  try {
+    const enMap = validNS.reduce(
+      (obj, ns) => ({
+        [ns]: gist.files[`${ns}.json`].raw_url,
+        ...obj
+      }),
+      {}
+    );
+    return {
+      en: enMap
+    };
+  } catch (e) {
+    throw new Error(`Cannot find locale files from github gists: ${e}`);
+  }
+}
+export const i18nReady = getJSONMap().then(jsonMap => {
+  return i18next.use(XHR).init({
+    debug: false,
+    lng: 'en',
+    fallbackLng: 'en',
+    ns: validNS,
+    defaultNS: validNS[0],
+    backend: {
+      loadPath: (lng, ns) => jsonMap[lng][ns],
+      crossDomain: true
+    }
+  });
+});
